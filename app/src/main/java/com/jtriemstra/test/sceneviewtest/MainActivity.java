4b7867d18c90579d8306c2db581c602fc5b0a4e2
Joel Triemstra <>
2020-02-17 18:58:29 -0500

    the pause and resume handlers were required to make the background color work?

--- app/src/main/java/com/jtriemstra/test/sceneviewtest/MainActivity.java
+++ app/src/main/java/com/jtriemstra/test/sceneviewtest/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.ar.sceneform.SceneView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -11,5 +13,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SceneView x = (SceneView) findViewById(R.id.scene_view);
+        x.pause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SceneView x = (SceneView) findViewById(R.id.scene_view);
+        try {
+            x.resume();
+        }
+        catch (Exception e){
+            
+        }
     }
 }
